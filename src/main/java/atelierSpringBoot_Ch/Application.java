53da57bdba12268772e1d4ee8b22aa4d7c257eb9
Rania <raniachihaoui38@gmail.com>
2024-11-22 00:29:57 +0100

    Ajouter des Modifications

--- src/main/java/atelierSpringBoot_Ch/Application.java
+++ src/main/java/atelierSpringBoot_Ch/Application.java
@@ -1,13 +1,19 @@
 package atelierSpringBoot_Ch;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class Application {
-
+public class Application implements CommandLineRunner{
+	  @Autowired
+	    private EmailService emailService;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	 @Override
+	    public void run(String... args) throws Exception {
+	        emailService.sendEmailAttachment("siwar.bengaji@gmail.com", "Reponse de Demmande", "Bonjour nous avons accepter votre demonade d'inscription vous pouver connecter votre compte ");
+	    }
 }
---
+++ src/main/java/atelierSpringBoot_Ch/EmailService.java
@@ -0,0 +1,74 @@
+package atelierSpringBoot_Ch;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.angus.mail.handlers.multipart_mixed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.Multipart;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+ 
+    
+   public void sendHtmlEmail() throws MessagingException, IOException{
+    	MimeMessage message =mailSender.createMimeMessage();
+    	message.setFrom(new InternetAddress("anyame@freelance.mailtrap.link"));
+    	message.setRecipients(MimeMessage.RecipientType.TO,"siwar.bengaji@gmail.com");
+    	String htmlTemplate = readFile("C:/edu.iset.atelierSpringBoot.Email/src/main/resources/template.html");
+    	String htmlContent = htmlTemplate.replace("${name}", "john Doe");
+    	htmlContent =htmlContent.replace("${message}","salem alaykom :)");
+    	
+    	
+    	message.setContent(htmlContent, "text/html; charset=utf-8");
+    	mailSender.send(message);
+    }
+    
+    public String readFile(String filePath) throws IOException {
+    	Path path=Paths.get(filePath);
+    	return Files.readString(path,StandardCharsets.UTF_8);
+    	
+    }
+    public void sendEmailAttachment(String to, String subject , String body) throws MessagingException{
+    	MimeMessage message= mailSender.createMimeMessage();
+    	MimeMessageHelper helper=new MimeMessageHelper(message, true);
+    	helper.setFrom("anyname@freelance.mailtrap.link");
+    	helper.setTo(to);
+    	helper.setSubject(subject);
+    	helper.setText(body);
+    	FileSystemResource file =new FileSystemResource(new File("C:\\Users\\21654\\Downloads\\download.png"));
+    	helper.addAttachment ("attachment.jpg", file);
+    	mailSender.send(message);
+    }
+    
+    //public void sendEmailWithAttachment(String to, String subject, String body) throws MessagingException {
+    	//MimeMessage message mailSender.createMimeMessage();
+    	//MimeMessageHelper helper = new MimeMessageHelper (message, multipart: true);
+    	//helper.setFrom("anyname@freelance.mailtrap.link");
+    //	helper.setTo(to);
+    	//helper.setSubject(subject);
+    ////	helper.setText(body);
+    	//FileSystemResource file = new FileSystemResource(new File(pathname: "/Users/ahenka/Downloads/attachment.jpg"));
+    	//helper.addAttachment (attachmentFilename: "attachment.jpg", file);
+    	//mailSender.send(message);
+    
+    
+}
